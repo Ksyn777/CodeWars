89a466b83ed98db4f1365c4f1d27a0c24ff5cd2a
Kamil Synowiec <kamilsynowiec18@gmail.com>
2026-01-06 17:01:27 +0100

    BooleanOrder

---
+++ 3kyu/theBooleanOrder.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdint>
+
+using namespace std;
+
+int64_t solve(const std::string &s, const std::string &ops)
+{
+  return 0;
+}
